7f8d94bac74dfe9fe1dbcf1444c8de06e87daaf8
kibarik <cvyatoslavka@gmail.com>
2022-04-11 14:42:24 +0300

    late commit

--- src/org/kibarik/exam/ui/ProductTableForm.java
+++ src/org/kibarik/exam/ui/ProductTableForm.java
@@ -6,14 +6,22 @@ import org.kibarik.exam.utils.CustomTableModel;
 import org.kibarik.exam.utils.DialogUtils;
 
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class ProductTableForm extends BaseForm{
     private JTable table;
     private JPanel mainPanel;
     private JButton addButton;
+    private JComboBox typeComboBox;
+    private JComboBox sortComboBox;
 
     CustomTableModel<ProductEntity> model;
 
@@ -21,10 +29,98 @@ public class ProductTableForm extends BaseForm{
         super(1000, 600);
         setContentPane(mainPanel);
 
+        initBoxes();
         initButtons();
         initTables();
 
         setVisible(true);
+    }
+
+//    public void initBoxes(){
+//        typeComboBox.addItemListener(e -> {
+//            if(e.getStateChange() == ItemEvent.SELECTED){
+//                applyFilters();
+//            }
+//        });
+//    }
+//
+//    public void applyFilters(){
+//        try {
+//            List<ProductEntity> list = ProductEntityManager.selectAll();
+//
+//            switch (typeComboBox.getSelectedIndex()) {
+//                case 1 -> list.removeIf(s -> Objects.equals(s.getProductType(), "Полумаски"));
+//                case 2 -> list.removeIf(s -> s.getProductType().equals("Защитные маски"));
+//            }
+//
+//            model.setRows(list);
+//            model.fireTableDataChanged();
+//
+//            if(list.isEmpty()){
+//                DialogUtils.showInfo(null, "В Таблице не осталось значений!");
+//            }
+//
+//        } catch (SQLException e) {
+//            DialogUtils.showError(null, "Ошибка получения данных.");
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+    public void initBoxes(){
+        typeComboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange()==ItemEvent.SELECTED){
+                    applyFilters();
+                }
+            }
+        });
+
+        sortComboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED){
+                    applyFilters();
+                }
+            }
+        });
+    }
+
+    public void applyFilters(){
+        try {
+            List<ProductEntity> list = ProductEntityManager.selectAll();
+            switch (typeComboBox.getSelectedIndex()) {
+                case 1 -> list.removeIf(p -> !p.getProductType().equals("Повязки"));
+                case 2 -> list.removeIf(p -> !p.getProductType().equals("Полумаски"));
+                case 3 -> list.removeIf(p -> !p.getProductType().equals("Маски"));
+            }
+
+            switch (sortComboBox.getSelectedIndex()){
+                case 1:
+                    Collections.sort(model.getRows(), new Comparator<ProductEntity>() {
+                        @Override
+                        public int compare(ProductEntity o1, ProductEntity o2) {
+                            return o1.getId() > o2.getId();
+                        }
+                    });
+                break;
+
+                case 2:
+                    Collections.sort(model.getRows(), new Comparator<ProductEntity>() {
+                        @Override
+                        public int compare(ProductEntity o1, ProductEntity o2) {
+                            return 0;
+                        }
+                    });
+            }
+
+        model.setRows(list);
+        model.fireTableDataChanged();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void initButtons(){
